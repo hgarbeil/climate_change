1de506486e5bd94bedd562528c1db46037702726
Harold Garbeil <hg@Harolds-MacBook-Pro.local>
2023-12-13 05:49:58 -1000

    pip style requirements.txt

--- process_co2.py
+++ process_co2.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import pandas_datareader as pdr
-import matplotlib.pyplot as plt
+# import pandas_datareader as pdr
+# import matplotlib.pyplot as plt
 import plotly.express as px
 import datetime
 import panel as pn
